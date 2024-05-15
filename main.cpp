7c9a0b97759d7d65a8b6139b1e1bb9deae38a51d
siuslawcarrie <102423932+siuslawcarrie@users.noreply.github.com>
2024-05-15 08:57:08 -0700

    working small bug
    it validates strings unless there is a space then it prints multiple messages

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@
 #include <random>
 #include <iomanip>
 #include <cctype>
+#include <cstdio>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
@@ -18,7 +21,10 @@ const int WIDTH = 20;//width for setw display
 //FUNCTION PROTOTYPES
 int random(int low, int high);//random number generator
 bool isString(const string s);//string validation
-string validation_string(string passed_string);//string validation
+int validation_integer(int min, int max);
+int monster_number();//get and validate number of monsters
+int value();
+string validation_string();
 
 //PROGRAM
 int main() {
@@ -28,13 +34,16 @@ int main() {
     string monster_name, monster_type = " ";//monster name
 //DISPLAY
     cout << "Welcome to the Monster Generator! Please enter a monster type: " << endl;
-    getline(cin, monster_name);// user to enter monster name/type
-    //validation_string(monster_name);
-    cout << "How many " << monster_name << "s do you want to generate? ";//ask user for number of monsters
-    cin >> num_monsters;//number of monsters
-    cout << "Now enter a value for each of these: monster health, monster magic power, ";//display
-    cout << "monster attack power, monster defense power (4 integers): " << endl;
-    cin >> health >> magic >> attack >> defense;//user entry, max random
+    monster_name = validation_string();
+    num_monsters = monster_number();//validate monster number
+    cout << "Please enter a max power for health: "<<endl;
+    cin>>health;
+    cout<<"and magic: "<<endl;
+    cin>>magic;
+    cout<<"now max attack: "<<endl;
+    cin>>attack;
+    cout<<"and max defense: ";
+    cin>>defense;
     cout << "You entered monster health: " << health << ", monster magic power: " << magic << ", monster attack power: "
          << attack << ", monster defense power: " << defense << endl;
     /***FOR TESTING****/
@@ -64,69 +73,138 @@ int main() {
     return 0;
 }
 
-//generate random numbers
-int random(int low, int high) {
-    return low + rand() % (high - low + 1);
-}
 
 //function to get and validate # of monsters
-int emp_id_func() {
-    cout << "Enter employee's ID:";
+int monster_number() {
+    cout << "How many monsters do you want to generate? ";//ask user for number of monsters
     int min = 1, max = 9999;
     return validation_integer(min, max);
+}
+//    //function to get and validate magic, health, attack, defense max
+//    int emp_id_func() {
+//        cout << "Enter employee's ID:";
+//        int min = 1, max = 9999;
+//        return validation_integer(min, max);
 
-    //function to get and validate magic, health, attack, defense max
-    int emp_id_func() {
-        cout << "Enter employee's ID:";
-        int min = 1, max = 9999;
-        return validation_integer(min, max);
-
-        //function to validate integers
-        int validation_integer(int min, int max) {
-            int number;
-            cin >> number;
-            while (!cin or (number < min) or (number > max)) {
+//int value(){
+//    for (int x = 1; x<=4; x++){
+//        int value;
+//        int min = 1, max = 9999;
+//        int count = 1;
+//        cout<<"Enter value "<<count<<":";
+//        cin>>value;
+//        count ++;
+//
+//    return validation_integer(min, max);
+//}
+    //function to validate integers
+    int validation_integer(int min, int max) {
+        int number;
+        cin >> number;
+        while (!cin or (number < min) or (number > max)) {
 // Explain error not entering an integer
-                cout << "I'm sorry that's not a valid entry. Please enter a number between " << min << " and " << max
-                     << ": ";
+            cout << "I'm sorry that's not a valid entry. Please enter a number between " << min << " and " << max
+                 << ": ";
 // Clear input stream
-                cin.clear();
+            cin.clear();
 // Discard previous input
-                cin.ignore(123, '\n');
-                cin >> number;
-                cout << endl;
-            }
-            return number;
+            cin.ignore(123, '\n');
+            cin >> number;
+            cout << endl;
         }
+        return number;
+    }
+
+string validation_string() {
+    string entered_string;
+bool valid = false;//valid labeling this as not correct
+    while (!valid){//valid now true
+        bool is_alpha = true;
+        cin >> entered_string;
+        int i=0;
+        while (is_alpha and i<entered_string.length()){
+
+            is_alpha = isalpha(entered_string [i]);
+           i++;
+        }
+        if (!cin or !is_alpha){
+            cout<<"Sorry, that is not a valid entry. Please try again: "<<endl;
+            cin.clear();
+// Discard previous input
+            cin.ignore(123, '\n');
+            cout << endl;
+        }
+        else{
+            valid = true;
+        }
+    }
+return entered_string;
+    /* isalpha example */
+
+//    int main ()
+//    {
+//        int i=0;
+//        char str[]="C++";
+//        while (str[i])
+//        {
+//            if (isalpha(str[i])) printf ("character %c is alphabetic\n",str[i]);
+//            else printf ("character %c is not alphabetic\n",str[i]);
+//            i++;
+//        }
+//        return 0;
+//    }
+
+    while (!cin) {
+        bool contains_non_alpha
+                = find_if(entered_string.begin(), entered_string.end(),
+                               [](char c) { return !std::isalpha(c); }) != entered_string.end();
+
+// Explain error not entering an integer
+        cout << "I'm sorry that's not a valid entry. Please try again. "<<endl;
+// Clear input stream
+        cin.clear();
+// Discard previous input
+        cin.ignore(123, '\n');
+        cin >> entered_string;
+        cout << endl;
+    }
+    return entered_string;
+}
 
 //function to validate monster name
-        string validation_string(string passed_string) //function to validate integers
-        {
-            string entered_string;
-            bool isString;
-            cin >> entered_string;
-//    isString(entered_string);
-            if (!isString) {
-// Explain error not entering a string
-                cout << "I'm sorry that's not a valid entry. Please try again. ";
-// Clear input stream
-                cin.clear();
-// Discard previous input
-                cin.ignore(123, '\n');
-                cin >> entered_string;
-                cout << endl;
-            }
-            return passed_string;
+//    string validation_string(string passed_string) //function to validate integers
+//    {
+//        string entered_string;
+//        isString;
+//        cin >> entered_string;
+////    isString(entered_string);
+//        if (!isString) {
+//// Explain error not entering a string
+//            cout << "I'm sorry that's not a valid entry. Please try again. ";
+//// Clear input stream
+//            cin.clear();
+//// Discard previous input
+//            cin.ignore(123, '\n');
+//            cin >> entered_string;
+//            cout << endl;
+//        }
+//        return passed_string;
+//    }
+//
+//    //validate string
+//    bool isString(const string s) {
+//        for (const char c: s) {
+//            if (!isalpha(c) and !isspace(c)) {
+//                return false;
+//            }
+//        }
+//    }
+        //generate random numbers
+        int random(int low, int high) {
+            return low + rand() % (high - low + 1);
         }
 
-        //validate string
-        bool isString(const string s) {
-            for (const char c: s) {
-                if (!isalpha(c) and !isspace(c)) {
-                    return false;
-                }
-            }
-        }
+
 
 
 
